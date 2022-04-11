be2bc5d4bdeb581710d223ee9fe1f6fdb1236d79
Bruno Totoli <santosbruno101062@gmail.com>
2022-04-11 20:58:32 -0300

    SimpleFileVisitor

---
+++ src/br/com/abc/javacore/Wnio/SimpleFileVisitorTest01.java
@@ -0,0 +1,23 @@
+package br.com.abc.javacore.Wnio;
+
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+
+class ListAllFiles extends SimpleFileVisitor<Path> {
+    @Override
+    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) {
+        if (file.getFileName().toString().endsWith(".java")) {
+            System.out.println(file.getFileName());
+        }
+        return FileVisitResult.CONTINUE;
+    }
+}
+
+
+public class SimpleFileVisitorTest01 {
+    public static void main(String[] args) throws IOException {
+        Path path = Paths.get("");
+        Files.walkFileTree(path, new ListAllFiles());
+    }
+}
