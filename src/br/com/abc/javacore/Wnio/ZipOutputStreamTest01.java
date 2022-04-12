fc0538cfdd7342f7410790f8f2f0670fd3481a8b
Bruno Totoli <santosbruno101062@gmail.com>
2022-04-12 10:45:54 -0300

    Fim NIO

---
+++ src/br/com/abc/javacore/Wnio/ZipOutputStreamTest01.java
@@ -0,0 +1,35 @@
+package br.com.abc.javacore.Wnio;
+
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+public class ZipOutputStreamTest01 {
+    public static void main(String[] args) {
+        Path arquivoZip = Paths.get("pasta/arquivo.zip");
+        Path arquivosParaZipar = Paths.get("pasta/subpasta");
+        zip(arquivoZip, arquivosParaZipar);
+
+    }
+
+    static void zip(Path arquivoZip, Path arquivosParaZipar) {
+        try (ZipOutputStream zipStream = new ZipOutputStream(Files.newOutputStream(arquivoZip));
+             DirectoryStream<Path> directoryStream = Files.newDirectoryStream(arquivosParaZipar)) {
+
+            for (Path file : directoryStream) {
+                ZipEntry zipEntry = new ZipEntry(file.getFileName().toString());
+                zipStream.putNextEntry(zipEntry);
+                Files.copy(file, zipStream);
+                zipStream.closeEntry();
+            }
+            System.out.println("Arquivo criado com sucesso");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
